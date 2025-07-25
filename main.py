60226a6ebd1cab4773e966676f8e959687997e73
ashraddhansh <ashraddhansh@protonmail.com>
2025-07-25 23:48:50 +0530

    add types for role and --verbose flag in input

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
@@ -12,8 +13,15 @@ def main():
         print("Usage: uv main.py \"Your prompt here\"")
         sys.exit(1)
     else:
-        output = client.models.generate_content(model = "gemini-2.0-flash-001",contents=sys.argv[1])
+        user_prompt = sys.argv[1]
+        messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+        ]
+        output = client.models.generate_content(model = "gemini-2.0-flash-001",contents=messages)
         print(output.text)
+
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {output.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {output.usage_metadata.candidates_token_count}")
 
