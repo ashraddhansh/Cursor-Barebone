f58e804269e87b40c877360b83a214640bde78bd
ashraddhansh <ashraddhansh@protonmail.com>
2025-07-27 00:17:55 +0530

    add function declarations and add ability to run functions in LLM

--- main.py
+++ main.py
@@ -3,10 +3,29 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
+
+
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
+
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
 
 def main():
     if len(sys.argv) == 1:
@@ -17,7 +36,12 @@ def main():
         messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
         ]
-        output = client.models.generate_content(model = "gemini-2.0-flash-001",contents=messages)
+        output = client.models.generate_content(model = "gemini-2.0-flash-001",contents=messages, config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt))
+
+        if output.function_calls:
+            function_call_part = types.FunctionCall
+            for function_call_part in output.function_calls:
+                print(f"Calling function: {function_call_part.name}({function_call_part.args})")
         print(output.text)
 
     if "--verbose" in sys.argv:
